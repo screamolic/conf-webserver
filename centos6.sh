b9e88b8661be7490e88a55073698815f2dce3213
Endyanto Dwi Saputro <screamolic@users.noreply.github.com>
2017-05-05 10:03:03 +0700

    Update centos6.sh

--- centos6.sh
+++ centos6.sh
@@ -8,18 +8,15 @@ fi
 
 # Define echo function
 # Blue color
-function ee_lib_echo()
-{
+function ee_lib_echo(){
     echo $(tput setaf 4)$@$(tput sgr0)
 }
 # White color
-function ee_lib_echo_info()
-{
+function ee_lib_echo_info(){
     echo $(tput setaf 7)$@$(tput sgr0)
 }
 # Red color
-function ee_lib_echo_fail()
-{
+function ee_lib_echo_fail(){
     echo $(tput setaf 1)$@$(tput sgr0)
 }
 
