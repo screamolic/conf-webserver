7ee8622375d5cb045274df2c6f9b3a25e47abefe
Endyanto Dwi Saputro <screamolic@users.noreply.github.com>
2017-07-15 12:08:01 +0700

    Create centos7.sh

---
+++ centos7.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+clear
+# Checking permissions
+if [[ $EUID -ne 0 ]]; then
+   ee_lib_echo_fail "Sudo privilege required..."
+   exit 100
+fi
+
+# Define echo function
+# Blue color
+function ee_lib_echo()
+{
+   echo $(tput setaf 4)$@$(tput sgr0)
+}
+# White color
+function ee_lib_echo_info()
+{
+   echo $(tput setaf 7)$@$(tput sgr0)
+}
+# Red color
+function ee_lib_echo_fail()
+{
+   echo $(tput setaf 1)$@$(tput sgr0)
+}
+
+# Execute: update
+ee_lib_echo "Updating, please wait..."
+yum -y update
+
+# Execute: installing
+ee_lib_echo "Installing webserver, please wait..."
+yum -y install httpd
+service httpd start
+/sbin/iptables -I INPUT -p tcp --dport 80 -j ACCEPT
+/etc/rc.d/init.d/iptables save
+yum -y install php php-common php-dev php-xml php-mbstring php-pear php-pecl-geoip php-devel geoip geoip-devel gcc make unzip curl wget htop
+sudo pecl install geoip
+rm -f /etc/httpd/conf/httpd.conf
+wget -O /etc/httpd/conf/httpd.conf https://raw.githubusercontent.com/screamolic/conf-webserver/master/httpd.conf
+
+echo -e "; Enable geoip extension module\nextension=geoip.so\n" >> /etc/php.d/geoip.ini
+
+#d install geoip
+cd /usr/share/GeoIP
+wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
+wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+gunzip GeoIP.dat.gz
+gunzip GeoLiteCity.dat.gz
+mv GeoLiteCity.dat GeoIPCity.dat
+
+# Php version
+echo -n "[In progress] Detect PHP version ..."
+VER_PHP="$(command php --version 2>'/dev/null' \
+   | command head -n 1 \
+   | command cut --characters=5-7)"
+sleep 3s
+echo -e "\r\e[0;32m[OK]\e[0m Detect PHP version  : $VER_PHP   "
+
+# Execute: installing ioncube
+ee_lib_echo "Installing ioncube, please wait..."
+cd /var/www/html
+wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+tar xvfz ioncube*
+cp /var/www/html/ioncube/ioncube_loader_lin_${VER_PHP}.so /usr/lib64/php/modules
+echo "zend_extension = /usr/lib64/php/modules/ioncube_loader_lin_${VER_PHP}.so" >> /etc/php.d/00-ioncube.ini
+service httpd restart
+clear
+ee_lib_echo "Cek Spesifikasi:"
+php -v
+httpd -v
