95ca3c8715d2588e3960691f11dbdf767519ac94
Endyanto Dwi Saputro <screamolic@users.noreply.github.com>
2018-10-30 08:58:37 +0700

    Update centos7.sh

--- centos7.sh
+++ centos7.sh
@@ -31,7 +31,16 @@ yum group install "Development Tools" -y
 # Execute: installing
 ee_lib_echo "Installing webserver, please wait..."
 yum -y install httpd wget
-yum -y install composer
+
+# install php 7
+ee_lib_echo "Installing PHP 7, please wait..."
+yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum install -y yum-utils nano wget git unzip gcc make curl htop fail2ban epel-release mod_ssl python-certbot-apache composer
+yum-config-manager --enable remi-php71
+yum install -y php php-common php-dev php-xml php-mbstring php-pear php-pecl-geoip php-devel geoip geoip-devel php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo
+rm -f /etc/httpd/conf/httpd.conf
+curl -o /etc/httpd/conf/httpd.conf https://raw.githubusercontent.com/screamolic/conf-webserver/master/httpd-7.conf
 
 /sbin/iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 /etc/rc.d/init.d/iptables save
@@ -44,17 +53,6 @@ sudo firewall-cmd --runtime-to-permanent
 sudo iptables -I INPUT -p tcp -m tcp --dport 80 -j ACCEPT
 sudo iptables -I INPUT -p tcp -m tcp --dport 443 -j ACCEPT
 service httpd start
-
-# install php 7
-ee_lib_echo "Installing PHP 7, please wait..."
-yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-yum install -y yum-utils nano wget git unzip gcc make curl htop fail2ban epel-release mod_ssl python-certbot-apache
-yum-config-manager --enable remi-php71
-yum install -y php php-common php-dev php-xml php-mbstring php-pear php-pecl-geoip php-devel geoip geoip-devel php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo
-sudo pecl install geoip
-rm -f /etc/httpd/conf/httpd.conf
-wget -O /etc/httpd/conf/httpd.conf https://raw.githubusercontent.com/screamolic/conf-webserver/master/httpd-7.conf
 
 # Php version
 echo -n "[In progress] Detect PHP version ..."
