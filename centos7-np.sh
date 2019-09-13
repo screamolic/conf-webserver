a453745cfcc291e0796aea0637054fb42cf20a51
Endyanto Dwi Saputro <screamolic@users.noreply.github.com>
2019-09-13 12:50:52 +0700

    Update centos7-np.sh

--- centos7-np.sh
+++ centos7-np.sh
@@ -27,10 +27,7 @@ function ee_lib_echo_fail()
 ee_lib_echo "Updating, please wait..."
 yum -y update
 yum group install "Development Tools" -y
-
-# Execute: installing
-ee_lib_echo "Installing webserver, please wait..."
-yum -y install nginx wget
+yum install -y wget
 
 # install php 7
 ee_lib_echo "Installing PHP 7, please wait..."
@@ -39,6 +36,11 @@ yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 yum install -y yum-utils nano wget git unzip gcc make curl htop fail2ban epel-release mod_ssl python-certbot-apache composer
 yum-config-manager --enable remi-php72
 yum install -y php php-common php-dev php-xml php-mbstring php-pear php-fpm php-pecl-geoip php-devel geoip geoip-devel php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo
+
+
+# Execute: installing
+ee_lib_echo "Installing webserver, please wait..."
+yum install -y nginx
 rm -f /etc/nginx/nginx.conf
 curl -o /etc/nginx/nginx.conf https://raw.githubusercontent.com/screamolic/conf-webserver/master/nginx.conf
 
